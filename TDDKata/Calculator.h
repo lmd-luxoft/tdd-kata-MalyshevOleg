d5d42ae8f2018dec46e8e9501f55960667727c0f
o.malyshev <oleg.malyshev11@gmail.com>
2020-12-01 10:50:08 +0300

    Tests well ok.

--- TDDKata/Calculator.h
+++ TDDKata/Calculator.h
@@ -5,6 +5,8 @@
 /// </summary>
 class Calculator
 {
+public:
+	enum {errEmpty=-1,errTooManyArgs=-2,errNotDigitArgs=-3,errIncorrectDelim=-4,errEnclosedDelim=-5};
 	/// <summary>
 	/// Sum of elements expression
 	/// </summary>
--- TDDKata/test.cpp
+++ TDDKata/test.cpp
@@ -1,5 +1,92 @@
 #include "pch.h"
-	TEST(TDDKata, TestName) {
-		ASSERT_EQ(1, 1);
-		ASSERT_TRUE(true);
+
+// correction test
+TEST(TDDKata, AddEmpyString) {
+	// Arrange
+		char * empty = "";
+		Calculator calc;
+		int res;
+	// Act
+		res=calc.Add(empty);
+	// ASSERT
+		ASSERT_EQ(res, (int)Calculator::errEmpty);
 	}
+
+// correction tests
+TEST(TDDKata, AddTooManyArgs) {
+		// Arrange
+		char* string = "1,2,3,4";
+		Calculator calc;
+		int res;
+		// Act
+		res = calc.Add(string);
+		// ASSERT
+		ASSERT_EQ(res, (int)Calculator::errTooManyArgs);
+	}
+// correction tests
+TEST(TDDKata, AddNotDigitArgs) {
+		// Arrange
+		char* string = "aaa";
+		Calculator calc;
+		int res;
+		// Act
+		res = calc.Add(string);
+		// ASSERT
+		ASSERT_EQ(res, (int)Calculator::errNotDigitArgs);
+		//		ASSERT_FALSE(true);
+	}
+
+// correction tests
+TEST(TDDKata, AddIncorrectDelim) {
+	// Arrange
+	char* string = "4;2";
+	Calculator calc;
+	int res;
+	// Act
+	res = calc.Add(string);
+	// ASSERT
+	ASSERT_EQ(res, (int)Calculator::errIncorrectDelim);
+	//		ASSERT_FALSE(true);
+}
+
+
+// correction tests
+TEST(TDDKata, AddUnclosedDelim) {
+	// Arrange
+	char* string = "4,";
+	Calculator calc;
+	int res;
+	// Act
+	res = calc.Add(string);
+	// ASSERT
+	ASSERT_EQ(res, (int)Calculator::errEnclosedDelim);
+	//		ASSERT_FALSE(true);
+}
+
+
+// work normal tests
+
+TEST(TDDKata, AddNotCorrectOne) {
+	// Arrange
+	char* string = "1";
+	Calculator calc;
+	int res;
+	// Act
+	res = calc.Add(string);
+	// ASSERT
+	ASSERT_EQ(res, (int)1);
+	//		ASSERT_FALSE(true);
+}
+
+// work normal tests
+TEST(TDDKata, AddNotCorrectTwo) {
+	// Arrange
+	char* string = "2,2";
+	Calculator calc;
+	int res;
+	// Act
+	res = calc.Add(string);
+	// ASSERT
+	ASSERT_EQ(res, (int)4);
+	//		ASSERT_FALSE(true);
+}
